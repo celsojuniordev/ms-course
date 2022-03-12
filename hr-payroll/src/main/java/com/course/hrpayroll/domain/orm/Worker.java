1ec6d378d7b40cf7750a6685ab6b6d98d718395a
Celso Junior <celsoestercio.jr@gmail.com>
2022-03-12 15:44:53 -0300

    rest template

---
+++ hr-payroll/src/main/java/com/course/hrpayroll/domain/orm/Worker.java
@@ -0,0 +1,17 @@
+package com.course.hrpayroll.domain.orm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Worker implements Serializable {
+
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+}
--- hr-payroll/src/main/java/com/course/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/course/hrpayroll/service/PaymentService.java
@@ -1,12 +1,30 @@
 package com.course.hrpayroll.service;
 
 import com.course.hrpayroll.domain.orm.Payment;
+import com.course.hrpayroll.domain.orm.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
     public Payment getPayment(Long workerId, Integer days) {
-        return new Payment("Bob", 200.0, days);
+
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", workerId.toString());
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
