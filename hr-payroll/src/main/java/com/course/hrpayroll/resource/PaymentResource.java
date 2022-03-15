91f9c0458278a40c77b496707f40d2f2a75c00b3
Celso Junior <celsoestercio.jr@gmail.com>
2022-03-14 22:36:04 -0300

    hystrix foult tolerance

--- hr-payroll/src/main/java/com/course/hrpayroll/resource/PaymentResource.java
+++ hr-payroll/src/main/java/com/course/hrpayroll/resource/PaymentResource.java
@@ -2,9 +2,13 @@ package com.course.hrpayroll.resource;
 
 import com.course.hrpayroll.domain.orm.Payment;
 import com.course.hrpayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/payments")
@@ -13,8 +17,13 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable("workerId") Long workerId, @PathVariable("days") Integer days) {
         return ResponseEntity.ok(service.getPayment(workerId, days));
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        return ResponseEntity.ok(new Payment("Bran", 400.0, days));
     }
 }
