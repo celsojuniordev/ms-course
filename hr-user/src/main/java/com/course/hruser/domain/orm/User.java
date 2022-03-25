bf347f940c5eb2a89651c50291e5a5412872c4bb
Celso Junior <celsoestercio.jr@gmail.com>
2022-03-24 23:46:46 -0300

    User, Role and many to many assossiation

---
+++ hr-user/src/main/java/com/course/hruser/domain/orm/User.java
@@ -0,0 +1,33 @@
+package com.course.hruser.domain.orm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
